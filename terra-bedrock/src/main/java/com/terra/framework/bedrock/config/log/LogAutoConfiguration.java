14da56001ca0c144b96111bbc08b9885a8da633f
yangwentao02 <yangwentao02@58.com>
2025-05-15 18:54:21 +0800

    refactor(bedrock): 移除日志自动配置中的追踪 ID 提供逻辑
    - 删除了 LogAutoConfiguration 中的 mdcTraceIdProvider 和 tracingTaskDecorator 方法
    - 移除了 MDCTraceIdProvider 和 TraceIdProvider 类
    -简化了 LogAutoConfiguration 类，仅保留 logPattern 方法

--- terra-bedrock/src/main/java/com/terra/framework/bedrock/config/log/LogAutoConfiguration.java
+++ terra-bedrock/src/main/java/com/terra/framework/bedrock/config/log/LogAutoConfiguration.java
@@ -1,12 +1,8 @@
 package com.terra.framework.bedrock.config.log;
 
-import com.terra.framework.bedrock.trace.MDCTraceIdProvider;
-import com.terra.framework.bedrock.trace.UUIDTraceId;
-import com.terra.framework.bedrock.trace.async.TraceRunnable;
 import com.terra.framework.common.log.LogPattern;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.TaskDecorator;
 
 public class LogAutoConfiguration {
 
@@ -16,15 +12,5 @@ public class LogAutoConfiguration {
         return new LogPattern();
     }
 
-    @Bean
-    public MDCTraceIdProvider mdcTraceIdProvider() {
-        return new MDCTraceIdProvider();
-    }
-
-
-    @Bean
-    public TaskDecorator tracingTaskDecorator(MDCTraceIdProvider traceIdProvider) {
-        return (task) -> new TraceRunnable(task, traceIdProvider.getIfAvailable(UUIDTraceId::create));
-    }
 
 }
