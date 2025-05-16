8ffc2667bf1c274a44844bb638aaba3e6b297944
yangwentao02 <yangwentao02@58.com>
2025-05-16 09:06:08 +0800

    feat(stream): 添加 RabbitMQ 消息队列支持
    - 新增 RabbitMQ 消息生产者、消费者和队列实现
    - 添加默认消息队列工厂和消息实现类
    - 实现消息监听和发布注解处理器
    - 添加相关配置属性和自动配置类

--- terra-nova/src/main/java/com/terra/framework/nova/tuner/model/OptimizationStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.terra.framework.nova.tuner.model;
-
-public enum OptimizationStrategy {
-    BAYESIAN_OPTIMIZATION,
-    EVOLUTIONARY_ALGORITHM,
-    GRADIENT_DESCENT,
-    RANDOM_SEARCH,
-    GRID_SEARCH
-} 
