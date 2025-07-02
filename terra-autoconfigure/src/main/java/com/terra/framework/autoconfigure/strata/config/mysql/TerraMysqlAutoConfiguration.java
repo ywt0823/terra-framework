4ce54588963c812dd54f5e56112ba00d8b7fc130
Zues <1475699607@qq.com>
2025-07-02 22:02:40 +0800

    Update TerraMysqlAutoConfiguration to use spring.datasource.url prefix

--- terra-autoconfigure/src/main/java/com/terra/framework/autoconfigure/strata/config/mysql/TerraMysqlAutoConfiguration.java
+++ terra-autoconfigure/src/main/java/com/terra/framework/autoconfigure/strata/config/mysql/TerraMysqlAutoConfiguration.java
@@ -1,11 +1,9 @@
 package com.terra.framework.autoconfigure.strata.config.mysql;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
@@ -20,17 +18,10 @@ import javax.sql.DataSource;
  * @date 2022年12月24日 22:05
  */
 @ConditionalOnClass(DataSourceProperties.class)
-@ConditionalOnProperty(prefix = "spring.datasource.druid", name = "url")
+@ConditionalOnProperty(prefix = "spring.datasource", name = "url")
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class TerraMysqlAutoConfiguration {
 
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.druid")
-    @Primary
-    public DataSource dataSource() {
-        return new DruidDataSource();
-    }
 
     @Bean
     @Primary
